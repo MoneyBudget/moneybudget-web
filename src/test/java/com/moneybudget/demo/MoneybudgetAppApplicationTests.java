fd764e601d483a52943ae6aa171fccf6e7c0bafd
Lazo07 <noreenlazo07@gmail.com>
2018-06-05 23:12:20 +0800

    Initial commits. Spring boot webapp skeleton

---
+++ src/test/java/com/moneybudget/demo/MoneybudgetAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moneybudget.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneybudgetAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
